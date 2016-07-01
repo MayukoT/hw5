29660235dd5d4e3a452366f41abd976a9ac42d60
MayukoT <tsupita@MTanemura-no-MacBook-Pro.local>
2016-07-01 13:13:45 +0900

    temp

--- python/patatokukasy/main.py
+++ python/patatokukasy/main.py
@@ -15,11 +15,52 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, html, values={}):
+        template = JINJA_ENVIRONMENT.get_template(html)
+        self.response.write(template.render(values))
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('moshimoshi, Hello world!')
+        self.render("main.html")
+        #self.response.write('moshimoshi, Hello world!')
+
+class Resultpage(BaseHandler):
+    def get(self):
+        word1 = self.request.get('word1')
+        word2 = self.request.get('word2')
+        if word1 is None or word2 is None:
+            self.redirect('/')
+        len1=len(word1)
+        len2=len(word2)
+        for(a,b)in zip(word1,word2):
+    #    if i<len1
+            self.response.out.write(a)
+            self.response.out.write(b)
+    #        if j<len2
+    #            self.response.out.write(word2[0])
+    #            j+=1
+    #            i+=1
+    #        if j<len2
+    #            j+=1
+    #
+    #    if(len(combine1)>len(combine2)):
+    #        combine=combine1
+    #    else:
+    #        combine=combine2
+    #    self.response.out.write(combine)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/result', Resultpage)
 ], debug=True)
